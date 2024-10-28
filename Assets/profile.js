046b49e3ad82ddcacda60cd6683fec23c6ec22c8
Austin Ryu <88960218+ryuaus26@users.noreply.github.com>
2024-10-27 18:55:03 -0700

    Update profile.js

--- Assets/profile.js
+++ Assets/profile.js
@@ -1,12 +1,6 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB5pHK1U6Oy5Ta9oPOcL5LfWXGP_U3838E",
-    authDomain: "liftascend.firebaseapp.com",
-    projectId: "liftascend",
-    storageBucket: "liftascend.appspot.com",
-    messagingSenderId: "403461421933",
-    appId: "1:403461421933:web:52452b598fb853c3cb3864",
-    measurementId: "G-RFR3H01R2N"
+     //YOUR API
 };
 let currentWeightUnit = 'lbs'; // Default unit is pounds
 // Initialize Firebase

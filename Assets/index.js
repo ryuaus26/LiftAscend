1fa91469be9b38f8dc7d2d8f78209800a02165b6
ryuaus26 <yunryu1217@gmail.com>
2024-10-28 17:49:54 -0700

    Fixed transition from porfile to home/login

--- Assets/index.js
+++ Assets/index.js
@@ -1,8 +1,12 @@
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  //YOUR API
+    apiKey: "AIzaSyB5pHK1U6Oy5Ta9oPOcL5LfWXGP_U3838E",
+    authDomain: "liftascend.firebaseapp.com",
+    projectId: "liftascend",
+    storageBucket: "liftascend.appspot.com",
+    messagingSenderId: "403461421933",
+    appId: "1:403461421933:web:52452b598fb853c3cb3864",
+    measurementId: "G-RFR3H01R2N"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
